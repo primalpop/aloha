b09cfa8018f429db296da5a05d638651c3f11207
parth <parth.buch.115@gmail.com>
2012-03-09 10:27:37 +0530

    working logout

--- allotter/urls.py
+++ allotter/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('allotter.views',
-	url(r'^$', 'hello'),
+	url(r'^$', 'apply'),
     url(r'^login/$', 'user_login'),
+    url(r'^logout/$', 'user_logout'),
     url(r'^register/$', 'user_register'),
     url(r'^apply/$', 'apply'),
 	url(r'^quit/$', 'quit'),
 	url(r'^save/$', 'save'),
+
 )
