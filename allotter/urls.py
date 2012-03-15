66efbff2ab8b1572b111f8ea7f6f98b885529a17
Primal Pappachan <primal007@gmail.com>
2012-03-15 13:25:52 +0530

    added the login url, commented the rest out

--- allotter/urls.py
+++ allotter/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('allotter.views',
-	url(r'^$', 'apply'),
     url(r'^login/$', 'user_login'),
-    url(r'^logout/$', 'user_logout'),
-    url(r'^register/$', 'user_register'),
-    url(r'^apply/$', 'apply'),
-	url(r'^quit/$', 'quit'),
-	url(r'^submit/$', 'submit'),
+    #url(r'^apply/$', 'apply'),
+	#url(r'^submit/$', 'submit'),
+    #url(r'^quit/$', 'quit'),
 )
