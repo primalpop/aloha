91d4fefc4f5c4c9e438fe08df9b28c2dbcd4f8a1
Primal Pappachan <primal007@gmail.com>
2012-03-25 00:31:07 +0530

    pdf generation and details page url added

--- allotter/urls.py
+++ allotter/urls.py
@@ -4,7 +4,9 @@ urlpatterns = patterns('allotter.views',
     url(r'^login/$', 'user_login'),
     url(r'^logout/$', 'user_logout'),
     url(r'^apply/$', 'apply'),
-    url(r'^(?P<reg_no>\w+)/submit/$', 'submit'), #change into numbers
+    url(r'^details/$', 'submit_details'),
+    url(r'^get_pdf/$', 'generate_pdf'),
+    url(r'^(?P<reg_no>\w+)/submit/$', 'submit_options'), #change into numbers
     url(r'^(?P<reg_no>\w+)/complete/$', 'complete'), #change into numbers
     url(r'^$', 'apply'),
     #url(r'^quit/$', 'quit'),
