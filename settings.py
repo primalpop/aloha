2b7590ea9ddb9a9882d9bde674996e47791a1fbf
Primal Pappachan <primal007@gmail.com>
2012-03-26 18:28:23 +0530

    added third party apps for dev

--- settings.py
+++ settings.py
@@ -103,6 +103,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'aloha.urls'
@@ -127,6 +128,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     #Third-Party Apps
     'south',
+    'django_extensions',
+    'debug_toolbar',
 
     'allotter',
 )
@@ -157,3 +160,18 @@ LOGGING = {
 
 AUTH_PROFILE_MODULE = "allotter.Profile"
 LOGIN_URL = '/allotter/login'
+
+#For developing locally
+INTERNAL_IPS = ('127.0.0.1')
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
