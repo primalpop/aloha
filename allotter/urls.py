76ef9a53536974b7f5bea95665029c99fd24456b
Primal Pappachan <primal007@gmail.com>
2012-03-26 18:11:31 +0530

    Added the reg no to all urls and changed the regex

--- allotter/urls.py
+++ allotter/urls.py
@@ -3,11 +3,9 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('allotter.views',
     url(r'^login/$', 'user_login'),
     url(r'^logout/$', 'user_logout'),
-    url(r'^apply/$', 'apply'),
-    url(r'^details/$', 'submit_details'),
-    url(r'^get_pdf/$', 'generate_pdf'),
-    url(r'^(?P<reg_no>\w+)/submit/$', 'submit_options'), #change into numbers
-    url(r'^(?P<reg_no>\w+)/complete/$', 'complete'), #change into numbers
-    url(r'^$', 'apply'),
-    #url(r'^quit/$', 'quit'),
+    url(r'^(?P<reg_no>\d+)/apply/$', 'apply'),
+    url(r'^(?P<reg_no>\d+)/details/$', 'submit_details'),
+    url(r'^(?P<reg_no>\d+)/get_pdf/$', 'generate_pdf'),
+    url(r'^(?P<reg_no>\d+)/submit/$', 'submit_options', name='submit_options'),
+    url(r'^(?P<reg_no>\d+)/complete/$', 'complete_allotment', name='complete_allotment'), 
 )
