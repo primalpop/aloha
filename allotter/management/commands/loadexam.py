ae8b9244c34131bcd8cd6d3bcf13c9828f7d1b03
parth <parth.buch.115@gmail.com>
2012-03-28 17:20:37 +0530

    normalized the csv reader, added opt_location

--- allotter/management/commands/loadexam.py
+++ allotter/management/commands/loadexam.py
@@ -1,17 +1,19 @@
+from optparse import make_option
 from csv import reader
 from django.core.management.base import BaseCommand, CommandError
 from allotter.models import Exam
 
 class Command(BaseCommand):
-    args = '<file_name...>'
+    option_list = BaseCommand.option_list + (
+        make_option('--o',metavar='Paper Code file name', type=str),
+    )
     help = "Give the filename of the csv file that has all the exam code and exam name relation"
     
     def handle(self, *args, **options):
         
         clean_exam()
         
-        for fname in args:
-            load_exam(fname)
+        load_exam(options)
         
         self.stdout.write('Done\n')
         
@@ -21,16 +23,16 @@ def clean_exam():
     data = Exam.objects.all()
     data.delete()
 
-def load_exam(filename):
+def load_exam(options):
     """Load exam code and exam name from the given csv file. The file should 
     declare a list of "exam_code;exam_name".
     """
     try:
-        csvFile = open(filename, 'rb')
+        csvFile = open(options.get('o'), 'rb')
     except IOError as (errno,strerror):
         print "I/O error({0}): {1}".format(errno, strerror)
         
-    csvReader = reader(csvFile, delimiter=";")
+    csvReader = reader(csvFile, delimiter=":")
     
     for data in csvReader:
         new_exam = Exam.objects.create()
